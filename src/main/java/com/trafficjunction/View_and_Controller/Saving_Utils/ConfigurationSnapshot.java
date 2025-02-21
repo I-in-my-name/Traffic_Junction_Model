807dc83852633fe6a8ed7c8bc96a914cb98ea48e
I-in-my-name <jack.e.d.llewellyn@gmail.com>
2025-02-21 18:33:41 +0000

    TDD for both careTaker and SnapShot, now completed

---
+++ src/main/java/com/trafficjunction/View_and_Controller/Saving_Utils/ConfigurationSnapshot.java
@@ -0,0 +1,18 @@
+package com.trafficjunction.View_and_Controller.Saving_Utils;
+
+import com.trafficjunction.JunctionConfiguration;
+
+public class ConfigurationSnapshot {
+
+    public JunctionConfiguration link;
+    int[] directionInfo;
+
+    public ConfigurationSnapshot(JunctionConfiguration link){
+        this.link = link;
+        directionInfo = link.getDirectionInfo().clone();
+    }
+    public void restore(){
+        link.setDirectionInfo(directionInfo);
+    }
+    
+}
